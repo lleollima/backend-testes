c0cbd97e60c2af2fe9c749db1eaa044d378b1576
lleollima <leonardolima2008@gmail.com>
2022-09-14 12:30:24 -0300

    backend nodeexpress

---
+++ index.js
@@ -0,0 +1,25 @@
+// server/index.js
+
+const express = require("express");
+
+const v1 = 1.96;
+const v2 = 1.04;
+
+const money = v1 + v2;
+
+const response = money.toLocaleString("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.json({ message: response });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
